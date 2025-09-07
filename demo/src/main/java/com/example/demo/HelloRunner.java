16ba25752661503991cfe8164fab0c6959bc9e01
Yevhenii Butkevych <143118683+deLordeee@users.noreply.github.com>
2025-09-07 15:46:57 +0300

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/HelloRunner.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(2)
+public class HelloRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello from Spring Boot");
+    }
+}
